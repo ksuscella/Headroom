f3378c076b7dde8d99a78ee094d8f708ab00e2be
ksuscella <ksuscella@gmail.com>
2016-08-01 18:27:03 -0400

    Basic Before WS
    Basic Web setup before creating some REST services

--- web/js/play.js
+++ web/js/play.js
@@ -1,20 +1,25 @@
+
+
+    
 function letsPlay() {
     //code
     //var u = new SpeechSynthesisUtterance('Welcome to HeadRoom.  Lets explore the world around you');
     //speechSynthesis.speak(u);
     
-    var u = new SpeechSynthesisUtterance();
-    u.lang = 'en-US';
-    u.rate = 1.25;
-    u.pitch = .95;
-    txt = 'Welcome to HeadRoom';
-    u.text = txt;
-    showConsole(txt);
-    speechSynthesis.speak(u);
-    txt = 'Lets explore the world around you';
-    u.text = txt;
-    showConsole(txt);
-    speechSynthesis.speak(u);
+    speak('Welcome to HeadRoom');
+    speak('Lets explore the world around you');
+    speak('Today is a beautiful day, lets begin');
+    speak('');
+    
+    speak('Current Date and Time is ' + getFormatDate());
+    speak("Lets check what's around you");
+    
+}
+
+function getFormatDate() {
+    //Pull Back Date in correct "speaking" format
+    dt = dateFormat(new Date(), "dddd, mmmm dS, yyyy, h MM TT");
+    return dt;
 }
 
 function clearAlert() {
@@ -22,8 +27,25 @@ function clearAlert() {
     window.clearTimeout(timeoutID);
 }
 
+function speak(msg) {
+    var delay=1000; //1 second
+    var u = new SpeechSynthesisUtterance();
+    u.lang = 'en-US';
+    u.rate = 1.25;
+    u.pitch = .95;
+    u.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == 'Karen'; })[0];
+
+    setTimeout(function() {
+    //your code to be executed after 1 second
+        u.text = msg;
+        showConsole(msg);
+        speechSynthesis.speak(u);
+    }, delay);
+    
+}
+
 function showConsole(alerts) {
     //code
     var com = document.getElementById("findings").innerHTML;
-    document.getElementById("findings").innerHTML = com + "<p class='p-left'>" + Date() + " " + alerts + "</p>";
+    document.getElementById("findings").innerHTML = "<p class='p-left'>" + Date() + " " + alerts + "</p>" + com;
 }
