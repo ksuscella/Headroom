8e48fc8779c81543fe94930b7ed388e64658a1c0
ksuscella <ksuscella@gmail.com>
2016-08-13 00:45:49 -0400

    snapshot

--- web/js/play.js
+++ web/js/play.js
@@ -1,29 +1,71 @@
-var lat = 0;
-var lng = 0;
+var lat = 0;    //Track User's Location to find geoArticles
+var lng = 0;    //Track User's Location to find geoArticles
+var user_state='PLAY'; //Options: PLAY PAUSE CONTINUE
+var instr_pause = 'Press <b>PAUSE</b> to see the world around you';         //Need to move text to a file
+var instr_cont = 'Press <b>CONTINUE</b> to listen to the world around you'; //Need to move text to a file
+
+function speakSpeak() {
+    //*** LETS GET THIS STARTED ***
+    //Run through guiSetup to determine state of the button
+    //Then determine which method to execute...letsPlay(), letsPause() or letsConintue
     
+    guiSetup(); //Manage GUI State in case we need to pause
+    
+    if (user_state=='PLAY') {
+        letsPlay();      }      //Start speaking
+    if (user_state=='PAUSE') {
+        letsPause();     }      //Stop speaking
+    if (user_state=='CONTINUE') {
+        letsContinue();  }      //Continue speaking
+}
+
+
 function letsPlay() {
-    //code
-    //var u = new SpeechSynthesisUtterance('Welcome to HeadRoom.  Lets explore the world around you');
-    //speechSynthesis.speak(u);
-    
+
     speak('Welcome to HeadRoom');
+    speak('I am at beta version 0.1');
     speak('Lets explore the world around you');
-    speak('Today is a beautiful day, lets begin');
+    speak('Before we begin there are a couple of things to note');
+    speak('This version does not keep track of articles');
+    speak('As you listen to Head Room, it will randomly assign an article');
+    speak('This means for this version, you could get article duplication');
+    speak('This issue will be fixed in the next version');
+    speak('Thank you for listening, I really appreciate it!');
+    //speak('Today is a beautiful day, lets begin');
     speak('Current Date and Time is ' + getFormatDate());
+    geoSpeakLocate();
     
+
+}
+function geoSpeakLocate() {
+    //code
     if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function(position) {
             
               lat = position.coords.latitude
               lng = position.coords.longitude
               speak("I see your location is at " + round(lat,2) + " and " + round(lng,2));
-              //speak("Lets check what's around you");
+              speak("Lets check what's around you");
               findData();
         }
     )
     };
-
 }
+
+function letsPause() {
+    //code
+    showConsole('PAUSE');
+    window.speechSynthesis.pause();
+    window.speechSynthesis.cancel();
+}
+function letsContinue() {
+    //code
+    showConsole('CONTINUE');
+    speak('Glad to be back online');
+    //speak('Lets see what\'s around you');
+    geoSpeakLocate();
+}
+
 function getFormatDate() {
     //Pull Back Date in correct "speaking" format
     dt = dateFormat(new Date(), "dddd, mmmm dS, yyyy, h MM TT");
@@ -52,7 +94,10 @@ function speakAndDelay(msg, delay) {
 }
 
 function speak(msg) {
-    speakAndDelay(msg, 1000) //1 second
+    if (user_state == 'PLAY' || user_state=='CONTINUE') {
+        speakAndDelay(msg, 1000) //1 second
+    }
+    
 }
 
 function showConsole(alerts) {
@@ -122,4 +167,35 @@ function findArticle(page_id) {
   };
   xhttp.open("GET", "../?type=pageid&getPageID="+page_id, true);
   xhttp.send();
+}
+
+function guiSetup() {
+     //Manage Button State
+    var butt = document.getElementById("speak_button");
+    var instr = document.getElementById("instructions");
+    
+    test_state = butt.innerHTML; //evaluate button state
+    
+    if (test_state == 'PLAY') {
+        //Update Text Box & Start Speaking
+        user_state = 'PLAY';
+        butt.innerHTML = 'PAUSE';
+        butt.setAttribute("class", "btn btn-lg btn-danger");
+        instr.innerHTML = instr_pause;
+    }
+    if (test_state == 'PAUSE') {
+        //Stop Speaking & updates text/buttons
+        user_state = 'PAUSE';
+        butt.innerHTML = 'CONTINUE';
+        butt.setAttribute("class", "btn btn-lg btn-success");
+        instr.innerHTML = instr_cont;
+    }
+    if (test_state == 'CONTINUE'){
+        //Pick Up Where User Paused
+        user_state = 'CONTINUE';
+        butt.innerHTML = 'PAUSE';
+        butt.setAttribute("class", "btn btn-lg btn-danger");
+        instr.innerHTML = instr_pause;
+    }
+    
 }
